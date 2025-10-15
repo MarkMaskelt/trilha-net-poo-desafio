957dd7ecfe84ff66971283f07a9b53e4066c2136
Marcos Torres <marcos_torres00@outlook.com.br>
2025-10-15 15:58:15 -0300

    feat: add Smartphone, Iphone and Nokia class

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Iphone: ");
+Smartphone meuIphone = new Iphone(numero: "11991684074",modelo: "Modelo 0",imei: "123456789",memoria: 64);
+meuIphone.Ligar();
+meuIphone.InstalarAplicativo("Whatapp");
+
+Console.WriteLine("Nokia: ");
+Smartphone meuNokia = new Nokia(numero: "11991684074", modelo: "Modelo 1", imei: "012345678", memoria: 125);
+meuNokia.Ligar();
+meuNokia.InstalarAplicativo("Telegram");
